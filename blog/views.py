0dca1af573d2798e0dcb98b848c48864b6147215
AliMoradzadeh <moracode.master@gmail.com>
2023-06-12 14:53:29 +0330

    change posts list view to a CBV view

--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.core.paginator import Paginator
+from django.views.generic import ListView
 
-def post_list(request):
-    posts = Post.published.all()
-    
-    paginator = Paginator(posts, 3)
-    page_number = request.GET.get("page", 1)
-    try:
-        page_number = min(int(page_number), paginator.num_pages)
-    except ValueError:
-        page_number = 1
-    posts = paginator.page(page_number)
-    return render(request, 'blog/post/list.html', {'posts': posts})
+
+class PostListView(ListView):
+    """
+    Alternative post list view
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, status=Post.Status.PUBLISHED, publish__year=year, publish__month=month, publish__day=day)
